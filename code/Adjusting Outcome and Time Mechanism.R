48a96c7fe6b817fd902c89dbbb6137070db13f52
catherineli1220 <34583948+catherineli1220@users.noreply.github.com>
2018-02-05 20:06:28 -0800

    Added function to generated different outcome frequencies & generated Y's with different outcome frequencies

--- code/Adjusting Outcome and Time Mechanism.R
+++ code/Adjusting Outcome and Time Mechanism.R
@@ -5,6 +5,7 @@
 # PURPOSE: To play around with exposure and outcome mechanism, and confounding in "true" population
 # UPDATES: 01/22/2018: CL adapted code from main Case Control Simulation Program to play around 
 #                      with confounding and outcome
+#          02/05/2018: CL added function to generate different outcome frequencies
 ################################################################################################
 
 ####### PROGRAM START
@@ -19,6 +20,7 @@ library(Epi) # for case-cohort and density sampling designs
 library(survival) # for clogit analysis
 library(dplyr)
 library(MASS)
+library(AER) # for dispersion test https://www.rdocumentation.org/packages/AER/versions/1.2-5/topics/dispersiontest
 
 # Set Working Directory
 #setwd("~/Documents/PhD/Ahern GSR/Case Control Simulation") # Chris's directory
@@ -27,6 +29,11 @@ setwd("C:/Users/Catherine/Desktop/Case Control GSR") # Catherine's directory
 
 # Read in population data
 pop <- read.csv("data/population.data.csv", stringsAsFactors = F)
+
+# Turn dummy variables into factors
+pop$race <- factor(apply(pop[,8:12], 1, function(x) which(x == 1)), labels = colnames(pop[,8:12]))
+pop$age <- factor(apply(pop[,14:19], 1, function(x) which(x == 1)), labels = colnames(pop[,14:19]))
+pop$education <- factor(apply(pop[,20:26], 1, function(x) which(x == 1)), labels = colnames(pop[,20:26]))
 
 N <- 1000000
 
@@ -37,63 +44,79 @@ pop$X <- runif(N, 0, 365.25*10)
 #pop$X <- rnorm(N, 365.25*5, 365.25*1)# Think about other distributions as well
 
 # Generate Exposure
-baseline_A <- log(0.1)
+baseline_A <- log(0.4)
 set.seed(2)
 pop$A <- rbinom(N, size = 1, prob = expit(baseline_A + 
-                                            (log(20))*pop$black + 
-                                            (log(0.6))*pop$asian + 
+                                            (log(1))*pop$black + 
+                                            (log(1.1))*pop$asian + 
                                             (log(0.8))*pop$hispanic +
-                                            (log(1.2))*pop$otherrace + 
-                                            (log(2))*pop$age_18_24 +
-                                            (log(3))*pop$age_25_34 +
-                                            (log(2.5))*pop$age_35_44 +
-                                            (log(2))*pop$age_45_54 +
-                                            (log(1.5))*pop$age_55_64 +
-                                            (log(25))*pop$age_over64 +
-                                            (log(20))*pop$male +
-                                            (log(20))*pop$educ_ged +
-                                            (log(0.9))*pop$educ_hs + 
-                                            (log(0.8))*pop$educ_somecollege +
-                                            (log(0.7))*pop$educ_associates +
-                                            (log(0.6))*pop$educ_bachelors +
-                                            (log(0.5))*pop$educ_advdegree))
+                                            (log(0.9))*pop$otherrace + 
+                                            (log(1.1))*pop$age_25_34 +
+                                            (log(1.2))*pop$age_35_44 +
+                                            (log(1.3))*pop$age_45_54 +
+                                            (log(1.4))*pop$age_55_64 +
+                                            (log(3))*pop$age_over64 +
+                                            (log(3))*pop$male +
+                                            (log(3))*pop$educ_ged +
+                                            (log(1.2))*pop$educ_hs + 
+                                            (log(1.1))*pop$educ_somecollege +
+                                            (log(1))*pop$educ_associates +
+                                            (log(0.9))*pop$educ_bachelors +
+                                            (log(0.8))*pop$educ_advdegree))
 
 # Check Exposure Frequency
 summary(pop$A)
 
-# Generate Outcome as Function of Exposure and Covariates - Socio-Demographic Patterns Loosely Based on Incidence of Lung Cancer in U.S. (https://www.cdc.gov/cancer/lung/statistics/race.htm)
-trueOR <- log(2)
-baseline_Y <- log(0.00005)
-set.seed(3)
-pop$Y <- rbinom(N, size = 1, prob = expit(baseline_Y + 
-                                            trueOR*pop$A +
-                                            (log(20))*pop$black + 
-                                            (log(0.8))*pop$asian + 
-                                            (log(0.6))*pop$hispanic +
-                                            (log(1))*pop$otherrace + 
-                                            (log(1.2))*pop$age_18_24 +
-                                            (log(1.5))*pop$age_25_34 +
-                                            (log(2))*pop$age_35_44 +
-                                            (log(2.5))*pop$age_45_54 +
-                                            (log(3))*pop$age_55_64 +
-                                            (log(25))*pop$age_over64 +
-                                            (log(20))*pop$male +
-                                            (log(20))*pop$educ_ged +
-                                            (log(.8))*pop$educ_hs + 
-                                            (log(.7))*pop$educ_somecollege +
-                                            (log(.6))*pop$educ_associates +
-                                            (log(.5))*pop$educ_bachelors +
-                                            (log(.4))*pop$educ_advdegree +
-                                            (log(1.00000000000001))*pop$X))
+# Generate Outcome Function
+generate_outcome <- function(OR, baselineY) {
+  OR <- log(OR)
+  baseline_Y <- log(baselineY) # set to 0.06 for 20% (2.0 vs 2.8), 0.025 for 10% (2.0 vs. 2.9), 0.005 for 2% (2.0 vs. 3.09)
+  set.seed(3)
+  pop$Y <- rbinom(N, size = 1, prob = expit(baseline_Y + 
+                                              OR*pop$A +
+                                              (log(1.1))*pop$black + 
+                                              (log(0.9))*pop$asian + 
+                                              (log(1.1))*pop$hispanic +
+                                              (log(1))*pop$otherrace + 
+                                              (log(1.1))*pop$age_25_34 +
+                                              (log(1.2))*pop$age_35_44 +
+                                              (log(1.3))*pop$age_45_54 +
+                                              (log(1.4))*pop$age_55_64 +
+                                              (log(3))*pop$age_over64 +
+                                              (log(3))*pop$male +
+                                              (log(3))*pop$educ_ged +
+                                              (log(1.1))*pop$educ_hs + 
+                                              (log(1))*pop$educ_somecollege +
+                                              (log(.9))*pop$educ_associates +
+                                              (log(.8))*pop$educ_bachelors +
+                                              (log(.7))*pop$educ_advdegree)) 
+                                              #+ (log(1.00000000000001))*pop$X)) No longer including time in outcome mechanims
+  
+}
+
+pop$Y.20 <- generate_outcome(OR = 2, baselineY = 0.06) # for ~20% outcome frequency
+pop$Y.10 <- generate_outcome(OR = 2, baselineY = 0.025) # for ~10% outcome frequency
+pop$Y.2 <- generate_outcome(OR = 2, baselineY = 0.005) # for ~2% outcome frequency
+pop$Y.05 <- generate_outcome(OR = 2, baselineY = 0.0011) # for ~0.05% outcome frequency
 
 # Set all non-cases to time = end of study 
-pop$X[pop$Y==0] <- 365.25*10
+pop$X.20 <- ifelse(pop$Y.20==0, 365.25*10, pop$X)
+pop$X.10 <- ifelse(pop$Y.10==0, 365.25*10, pop$X)
+pop$X.2 <- ifelse(pop$Y.2==0, 365.25*10, pop$X)
+pop$X.05 <- ifelse(pop$Y.05==0, 365.25*10, pop$X)
 
 # Check Outcome Frequency
-summary(pop$Y)
+summary(pop$Y.20)
+summary(pop$Y.10)
+summary(pop$Y.2)
+summary(pop$Y.05)
+
+# Set X and Y to run following models
+pop$Y <- pop$Y.2
+pop$X <- pop$X.2
 
 # Logistic Model to Check Fidelity of Exposure/Outcome Relationship in Total Population
-mod <- glm(Y ~ A + black + asian + hispanic + otherrace + age_18_24 + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, data=pop, family='binomial')
+mod <- glm(Y ~ A + black + asian + hispanic + otherrace + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, data=pop, family='binomial')
 summary(mod)
 exp(coef(mod))
 
@@ -103,26 +126,34 @@ summary(crude_mod)
 exp(coef(crude_mod))
 
 # True Odds Ratio in total population
-trueOR_mod <- glm(Y ~ A + black + asian + hispanic + otherrace + age_18_24 + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, data=pop, family='binomial')
+trueOR_mod <- glm(Y ~ A + black + asian + hispanic + otherrace + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, data=pop, family='binomial')
 trueOR <- as.numeric(exp(coef(trueOR_mod)["A"])); trueOR
 
 # True Cumulative Incidence Ratio in total population
-# log binomial model
-#trueCIR_lbmod <- glm(Y ~ A + black + asian + hispanic + otherrace + age_18_24 + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, data=pop, family=binomial(log))
-#trueCIR_lb <- as.numeric(exp(coef(trueCIR_lbmod)["A"])); trueCIR_lb
+# log binomial model --> more likely to converge when outcome frequency is low?
+trueCIR_lbmod <- glm(Y ~ A + black + asian + hispanic + otherrace + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, data=pop, family=binomial(log))
+trueCIR_lb <- as.numeric(exp(coef(trueCIR_lbmod)["A"])); trueCIR_lb
 # poisson model
-trueCIR_pmod <- glm(Y ~ A + black + asian + hispanic + otherrace + age_18_24 + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, data=pop, family='poisson')
+trueCIR_pmod <- glm(Y ~ A + black + asian + hispanic + otherrace + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, data=pop, family='poisson')
 trueCIR_p <- as.numeric(exp(coef(trueCIR_pmod)["A"])); trueCIR_p
 
 # True Incidence Density Ratio in total population
 # Poisson Model with X offset
-trueIDR_mod <- glm(Y ~ A + black + asian + hispanic + otherrace + age_18_24 + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, offset = log(X), data=pop, family='poisson')
+trueIDR_mod <- glm(Y ~ A + black + asian + hispanic + otherrace + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, offset = log(X), data=pop, family='poisson')
 trueIDR <- as.numeric(exp(coef(trueIDR_mod)["A"])); trueIDR
+# Quasi-poisson model with X offset
+trueIDR_mod.qp <- glm(Y ~ A + black + asian + hispanic + otherrace + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree, offset = log(X), data=pop, family='quasipoisson')
+trueIDR.qp <- as.numeric(exp(coef(trueIDR_mod.qp)["A"])); trueIDR.qp
 # Negative Binomial Model
-#trueIDR_mod.nb <- glm.nb(Y ~ A + black + asian + hispanic + otherrace + age_18_24 + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree + offset(log(X)), data=pop, control=glm.control(maxit=100))
-#trueIDR.nb <- as.numeric(exp(coef(trueIDR_mod.nb)["A"]))
+trueIDR_mod.nb <- glm.nb(Y ~ A + black + asian + hispanic + otherrace + age_25_34 + age_35_44 + age_45_54 + age_55_64 + age_over64 + male + educ_ged + educ_hs + educ_somecollege + educ_associates + educ_bachelors + educ_advdegree + offset(log(X)), data=pop, control=glm.control(maxit=50))
+trueIDR.nb <- as.numeric(exp(coef(trueIDR_mod.nb)["A"]))
 
-## To save population data
+# Looking at dispersion
+dispersiontest(trueCIR_pmod)
+dispersiontest(trueIDR_mod)
+pchisq(2 * (logLik(trueIDR_mod) - logLik(trueIDR_mod.nb)), df = 1, lower.tail = FALSE)
+
+ ## To save population data
 #write.csv(pop, "data/population.data.csv", row.names=F)
   
 # END
