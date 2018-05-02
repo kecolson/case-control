14daea9b6ae4f969e91f04a287578a03e0045350
catherineli1220 <34583948+catherineli1220@users.noreply.github.com>
2018-05-02 10:07:05 -0700

    Fixed if else statement for desnity sampling with weights
    Removed ifelse() and replaced with two separate if else statements

--- code/ccwc.weights.R
+++ code/ccwc.weights.R
@@ -191,10 +191,12 @@ ccwc.weights <-function(entry=0, # time of entry to follow up
         fr[(nn+1):(nn+ncase)] <- 1
         fr[(nn+ncase+1):newnn] <- 0
         pr[(nn+1):(nn+ncase)] <- (1:n)[case]
-        pr[(nn+ncase+1):(newnn)] <- ifelse(is.null(weights), 
-                                           sample((1:n)[noncase], size=ncont, replace = FALSE), #Replace is FALSE is no weights passed in
-                                           sample((1:n)[noncase], size=ncont, replace = TRUE, #Replace is TRUE if weights passed in
-                                                  prob = noncaseweights)) #added sampling weights here
+        if (is.null(weights)) {
+          pr[(nn+ncase+1):(newnn)] <- sample((1:n)[noncase], size=ncont, replace = FALSE) #Replace is FALSE is no weights passed in
+        } else {
+          pr[(nn+ncase+1):(newnn)] <- sample((1:n)[noncase], size=ncont, replace = TRUE, #Replace is TRUE if weights passed in
+                                             prob = noncaseweights) #added sampling weights here
+        }
         nn <- newnn
       }
       else {
